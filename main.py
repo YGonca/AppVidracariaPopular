fd241c11fd8cd381fa838351ffc2646b15bee2e4
Yann Gonçalves <72891771+YGonca@users.noreply.github.com>
2024-03-21 01:40:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,49 +27,49 @@ Name = customtkinter.StringVar()
 Description = customtkinter.StringVar()
 Id = customtkinter.StringVar()
 
-def AddContact():
+def addContact():
     if Name.get() != "" and Description.get() != "":   
         cursor.execute("INSERT INTO contacts VALUES (?, ?, ?)", (Name.get(), Description.get(), uuid.uuid4().hex))
         connection.commit()
 
-        SetList()
-        EntryReset()
+        setList()
+        entryReset()
         messagebox.showinfo("Confirmação", "Sucesso ao adcionar cliente!")
 
     else:
         messagebox.showerror("Erro","Favor, preencha informações")
 
-def UpdateContact():
+def updateContact():
     if Name.get() != "" and Description.get() != "":
         cursor.execute(f"UPDATE contacts SET name = '{Name.get()}' WHERE id = '{Id.get()}'")
         cursor.execute(f"UPDATE contacts SET description = '{Description.get()}' WHERE id = '{Id.get()}'")
         connection.commit()
         messagebox.showinfo("Confirmação", "Sucesso ao atualizar Cliente")
-        EntryReset()
-        SetList()
+        setList()
+        entryReset()
     else:
         messagebox.showerror("Erro", "Favor, preencha informações")
 
-def DeleteContact():
+def deleteContact():
     if Name.get() != "" and Description.get() != "" and Id.get != "":
         result = messagebox.askyesno('Confirmação','Quer mesmo apagar?')
         if result == True:
             cursor.execute(f"DELETE FROM contacts WHERE id = '{Id.get()}'")
             connection.commit()
-            SetList()
+            setList()
     else:
         messagebox.showerror("Erro", "Favor, selecionar o Nome")
 
-def EntryReset():
+def entryReset():
     Name.set("")
     Description.set("")
     Id.set("")
 
-def Exit():
+def exit():
     connection.close()
     root.destroy()
 
-def ButtonClicked(id:str):
+def buttonClicked(id:str):
     cursor.execute(f"SELECT name, description FROM contacts WHERE id = '{id}'")
     list_contacts = cursor.fetchall()
 
@@ -86,21 +86,21 @@ label.pack(pady=12, padx=10)
 entryName = customtkinter.CTkEntry(master=frame, textvariable=Name, font=("Times new roman",20), width=500).place(x=30, y=100)
 entryDescription = customtkinter.CTkEntry(master=frame, textvariable=Description, font=("Times new roman",20), width=500).place(x=30, y=150)
 
-buttonAdd = customtkinter.CTkButton(master=frame, text="Adicionar", command=AddContact, font=("Times new roman",20)).place(x=30, y=200)
-buttonUpdate = customtkinter.CTkButton(master=frame, text="Atualizar", command=UpdateContact, font=("Times new roman",20)).place(x=30, y=250)
-buttonDelete = customtkinter.CTkButton(master=frame, text="Apagar", command=DeleteContact, font=("Times new roman",20)).place(x=180, y=250)
-buttonReset = customtkinter.CTkButton(master=frame, text="Resetar", command=EntryReset, font=("Times new roman",20)).place(x=180, y=200)
-buttonExit = customtkinter.CTkButton(master=frame, text="Sair", command=Exit, font=("Times new roman",20)).place(x=30, y=450)
+buttonAdd = customtkinter.CTkButton(master=frame, text="Adicionar", command=addContact, font=("Times new roman",20)).place(x=30, y=200)
+buttonUpdate = customtkinter.CTkButton(master=frame, text="Atualizar", command=updateContact, font=("Times new roman",20)).place(x=30, y=250)
+buttonDelete = customtkinter.CTkButton(master=frame, text="Apagar", command=deleteContact, font=("Times new roman",20)).place(x=180, y=250)
+buttonReset = customtkinter.CTkButton(master=frame, text="Resetar", command=entryReset, font=("Times new roman",20)).place(x=180, y=200)
+buttonExit = customtkinter.CTkButton(master=frame, text="Sair", command=exit, font=("Times new roman",20)).place(x=30, y=450)
 
-def SetList():
+def setList():
     cursor.execute("SELECT * FROM contacts ORDER BY name")
     list_contact = cursor.fetchall()
     frameSelect = customtkinter.CTkScrollableFrame(master=frame, height=350, width=250, label_text="List")
     frameSelect.place(x=600, y=100)
     for i, list in enumerate(list_contact):
-        new_button = customtkinter.CTkButton(master=frameSelect, text=list[0], command=lambda id=list[2]: ButtonClicked(id))
+        new_button = customtkinter.CTkButton(master=frameSelect, text=list[0], command=lambda id=list[2]: buttonClicked(id))
         new_button.pack(pady=10)
     
-SetList()
+setList()
 root.mainloop()
 connection.close()
